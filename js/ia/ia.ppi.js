e3dcc87b1bfaf63d9a412bbcc63d9a523093dc45
Peter Snyder <snyderp@gmail.com>
2013-03-18 13:40:34 -0500

    Tracking for states, cities, majors, etc.

---
+++ js/ia/ia.ppi.js
@@ -0,0 +1,135 @@
+if ('ppi' in window.ia === false) {
+    window.ia.ppi = {};
+}
+
+(function (ns) {
+
+    var PpiCheck = function (title, test_callback) {
+            return {
+                title: title,
+                check: function (value) {
+                    return test_callback(value);
+                }
+            };
+        },
+        data_checker_gen = function (data_set) {
+
+            var i, escape_index,
+                num_data = data_set.length,
+                // Convert the list of strings into a set of regexes, so that
+                // we can do case comparison easier
+                data_regexes = [],
+                escape_chars = ['.', '(', ')'],
+                an_escape_char,
+                data_escaped;
+
+            for (i = 0; i < num_data; i += 1) {
+                data_escaped = data_set[i];
+
+                for (escape_index in escape_chars) {
+                    an_escape_char = escape_chars[escape_index];
+                    data_escaped = data_escaped.replace(an_escape_char, '\\' + an_escape_char);
+                }
+
+                data_regexes.push(new RegExp(data_escaped, 'i'));
+            }
+
+            return function (value) {
+
+                var index,
+                    regex_match,
+                    current_regex,
+                    regex_matches = [];
+
+                for (index in data_regexes) {
+
+                    current_regex = data_regexes[index];
+                    regex_match = current_regex.exec(value);
+
+                    if (regex_match) {
+                        regex_matches.push(regex_match[0]);
+                    }
+                }
+
+                return regex_matches;
+            };
+        },
+        ppi = ns.ppi,
+        data = ns.data;
+
+    ppi.collection = [];
+
+    ppi.collection.push(PpiCheck("Year", (function () {
+
+        var year_regex = /\D?(\d{4})\D?/g;
+
+        return function (value) {
+
+            var regex_match,
+                regex_matches = [];
+
+            while (regex_match = year_regex.exec(value)) {
+
+                // Only accept year matches that have a non-digit before or
+                // after the year.
+                if (regex_match[0].length !== 4) {
+                    regex_matches.push(regex_match[1]);
+                }
+            }
+
+            return regex_matches;
+        };
+    }())));
+
+    ppi.collection.push(PpiCheck("Cities", data_checker_gen(data.cities)));
+    ppi.collection.push(PpiCheck("States", data_checker_gen(data.states)));
+    ppi.collection.push(PpiCheck("Majors and Minors", data_checker_gen(data.majors)));
+    ppi.collection.push(PpiCheck("Gender", data_checker_gen(['male', 'female', 'transgenered'])));
+
+    ppi.collection.push(PpiCheck("GPA", (function () {
+
+        var gpa_regex = /\D?(\d\.\d{1,2})\D?/g;
+
+        return function (value) {
+
+            var regex_match,
+                regex_matches = [],
+                float_version,
+                match;
+
+            while (regex_match = gpa_regex.exec(value)) {
+
+                float_version = +regex_match[1];
+
+                // Only accept year matches that have a non-digit before or
+                // after the year.
+                if (float_version > 0 && float_version < 5.01 && regex_match[0].length > 3) {
+                    regex_matches.push(regex_match[1]);
+                }
+            }
+
+            return regex_matches;
+        };
+    }())));
+
+    ppi.find_ppi = function (text) {
+
+        var ppi_check,
+            index,
+            found_ppi,
+            matches = {};
+
+        for (index in ppi.collection) {
+
+            ppi_check = ppi.collection[index];
+            found_ppi = ppi_check.check(text);
+
+            if (found_ppi.length > 0) {
+                matches[ppi_check.title] = found_ppi;
+            }
+        }
+
+        return matches;
+    };
+
+}(window.ia));
--- js/script.js
+++ js/script.js
@@ -1,49 +1,71 @@
-$(function () {
+(function ($, IA) {
+    'use strict';
 
-    var $content_areas = $("textarea.editable"),
-        year_regex = /\D([0-9]{4})\D/,
-        email_regex = /\s\w+@\w.\w\s/,
-        ppi_remove_regex = /<span class="ppi-marker">(.*?)<\/span>/mg,
-        trim = $.trim,
-        find_ppi = function (text) {
-            var year_matches = year_regex.exec(text);
-            return year_matches;
-        },
-        wrap_ppi = function (ppi) {
-            return '<span class="ppi-marker">' + trim(ppi) + "</span>";
-        },
-        remove_ppi_markers = function (html) {
-            var rs = html.replace(ppi_remove_regex, '$1');
-            return rs;
-        },
-        add_ppi_marker = function (src, str) {
-            var res = src.replace(str, wrap_ppi(str));
-            return res;
-        };
+    $(function () {
 
-    $content_areas.each(function () {
+        var $content_areas = $("textarea.editable"),
+            ppi = IA.ppi,
+            ppi_remove_regex = /<span class="ppi-marker">(.*?)<\/span>/mg,
+            font_tag_remove_regex = '/<font color=".*?">(.*?)<\/font>/mg',
+            trim = $.trim,
+            normalize_html = function (html) {
+                return html.replace("&nbsp;", " ");
+            },
+            wrap_ppi = function (ppi) {
+                return '<span class="ppi-marker">' + trim(ppi) + "</span>";
+            },
+            remove_ppi_markers = function (html) {
+                var rs = html
+                    .replace(ppi_remove_regex, '$1')
+                    .replace(font_tag_remove_regex, '$');
+                return rs;
+            },
+            add_ppi_marker = function (src, str) {
+                var res = src.replace(str, wrap_ppi(str));
+                return res;
+            };
 
-        var editor = CKEDITOR.replace(this.id),
-            $container = $(this).parent();
+        $content_areas.each(function () {
 
-        editor.on('blur', function (event) {
+            var editor = CKEDITOR.replace(this.id),
+                $container = $(this).parent();
 
-            var current_html = this.getData(),
-                has_ppi = find_ppi(current_html);
+            editor.on('blur', function (event) {
 
-            this.setData(remove_ppi_markers(current_html), function () {
+                var current_html = this.getData(),
+                    clean_html = normalize_html(current_html),
+                    found_ppi = ppi.find_ppi(clean_html);
 
-                if (has_ppi) {
-                    this.setData(add_ppi_marker(current_html, has_ppi[1]));
-                    $container.popover({
-                        placement: "top",
-                        title: "You done messed up",
-                        content: "You revealed all kinds of private stuff"
-                    }).popover('show');
-                } else {
-                    $container.popover('hide');
-                }
+                clean_html = remove_ppi_markers(clean_html);
+
+                this.setData(clean_html, function () {
+
+                    var ppi_title, ppi_type,
+                        ppi_instance_index, ppi_instance;
+
+                    if (found_ppi) {
+
+                        for (ppi_title in found_ppi) {
+                            ppi_type = found_ppi[ppi_title];
+                            for (ppi_instance_index in ppi_type) {
+                                ppi_instance = ppi_type[ppi_instance_index];
+                                clean_html = add_ppi_marker(clean_html, ppi_instance);
+                            }
+                        }
+
+                        this.setData(clean_html);
+                        $container.popover({
+                            placement: "top",
+                            title: "Warning",
+                            content: "You revealed all kinds of private stuff"
+                        }).popover('show');
+
+                    } else {
+
+                        $container.popover('hide');
+                    }
+                });
             });
         });
     });
-});
+}(jQuery, window.ia));
