c58c63c7f604b68806639f5c1443425484bed101
pete snyder <snyderp@bkps.defaultdomain>
2013-04-24 22:34:03 +0000

    Adding PPI columns to exported csv file

--- tools/export.py
+++ tools/export.py
@@ -3,6 +3,24 @@ import sys
 import datetime
 from pymongo import MongoClient
 
+ppi_types = (
+    'Year',
+    'Cities',
+    'States',
+    'Majors and Minors',
+    'Faculty',
+    'Gender',
+    'GPA',
+    'Class Number',
+    'Class Year',
+    'Sexual Orientation',
+    'Season',
+    'Letter Grade',
+    'Building (Abbr)',
+    'Building (Full)',
+)
+
+
 client = MongoClient()
 collection = client.igert.survey
 rows = []
@@ -15,16 +33,21 @@ for record in collection.find():
         if u'ppi_counts' in response:
             ppi_so_far[response[u'question']] = sum([int(v) for k, v in response[u'ppi_counts'].items()])
             cur_ppi = ", ".join(["%s: %s" % (k, v) for k, v in response[u'ppi_counts'].items()])
+            row_ppi_types = [response[u'ppi_counts'].get(ppi, 0) for ppi in ppi_types]
         else:
             ppi_so_far[response[u'question']] = 0
             cur_ppi = ""
-        rows.append(row + [
+            row_ppi_types = [0] * len(ppi_types)
+
+        row_values = [
             response[u'group'],
             response[u'question'],
             cur_ppi,
             sum(ppi_so_far.values()),
             datetime.datetime.fromtimestamp(float(response[u'time']) / 1000),
-        ])
+        ]
+
+        rows.append(row + row_values + row_ppi_types)
 
 
 writer = csv.writer(sys.stdout)
@@ -41,7 +64,21 @@ writer.writerow((
     "Question #",
     "PPI Revealed This Question",
     "# PPI Revealed So Far",
-    "Question Submitted"
+    "Question Submitted",
+    'Year',
+    'Cities',
+    'States',
+    'Majors and Minors',
+    'Faculty',
+    'Gender',
+    'GPA',
+    'Class Number',
+    'Class Year',
+    'Sexual Orientation',
+    'Season',
+    'Letter Grade',
+    'Building (Abbr)',
+    'Building (Full)',
 ))
 
 for row in rows:
